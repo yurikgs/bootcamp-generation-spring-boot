8d86ddc2276f63a880c4fb06f87948cce606a731
yurikgs <yurikgs@outlook.com>
2021-12-01 06:54:27 -0300

    first commit

---
+++ hello-word-spring-boot/hello/src/main/java/com/helloword/controller/helloController.java
@@ -0,0 +1,22 @@
+package com.helloword.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class helloController {
+	
+	@GetMapping 
+	public String hello() {
+		
+		return "Hello World /n/n"
+				+ "Habilidades e ementalidades que usei: atenção aos detalhes, "
+				+ "proatividade./n/n"
+				+ "Objetivos de Aprendizagem:"
+				+ "/n/n"
+				+ "Objetivo de aprendizagem essa semana: Persistência, Responsablidade Pessoal";
+	}
+	
+}
